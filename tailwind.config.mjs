371bf18a28941417311b43d7919ceb66e1011f27
HectorIdme <felipeidme04@gmail.com>
2023-11-02 13:44:34 -0500

    Added projects and skills, plus reponsive design

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			screens:{
+				'xs': {'min':'0px','max':'639px'},
+			}
+		},
 	},
 	plugins: [],
 }
